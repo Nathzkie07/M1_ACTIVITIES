3af645516db498351c5325ecca9de6495eae9d5d
Nathaniel F. Torrero <nftorrero@headoffice.corp.bpi.com.ph>
2025-12-04 11:11:44 +0800

    Activity 1

---
+++ inputYourName.java
@@ -0,0 +1,17 @@
+package javaTraining;
+import java.util.*;
+
+public class inputYourName {
+
+	public static void main(String[] args) {
+		
+		String name ="";
+		
+		try (Scanner input = new Scanner(System.in)) {
+			System.out.print("What is your name?:");
+			name= input.nextLine();
+		}	
+		
+		System.out.println("Hello "+name+"!");	
+	}
+}
